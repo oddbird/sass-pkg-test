25cfbc792d67342cd2d0f9f9809bdd5ea8d822da
James Stuckey Weber <jamesnw@gmail.com>
2023-06-30 09:14:41 -0400

    Add noPackage cases, types

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const assert = require('node:assert');
 const pkgResolution = require('./pkg-resolution');
 const line = () => console.log('\n\n====================')
 
+/** @typedef {[string, string|null]} TestCase */
+
+/** @param {TestCase} testCase  */
 function checkExample([pkgPath, expectedResolvedPath]) {
     let res = pkgResolution(pkgPath);
 
@@ -19,7 +22,8 @@ console.log('See `index.js` for comments, `node_modules` for mocked dependencies
 
 line();
 console.log('foo - simple module with no exports, "main" is in root\n');
-[
+/** @type TestCase[] */
+const fooCases = [
     ["foo", "/node_modules/foo"],
     // Attempting to load a non-valid file will fail
     ["foo/index.js", null],
@@ -29,12 +33,14 @@ console.log('foo - simple module with no exports, "main" is in root\n');
     ["foo/dist/scss", '/node_modules/foo/dist/scss'],
     ["foo/dist/scss/foo", '/node_modules/foo/dist/scss/foo'],
     ["foo/dist/scss/_index.scss", '/node_modules/foo/dist/scss/_index.scss']
-].forEach(example => checkExample(example))
+];
+fooCases.forEach(checkExample);
 
 
 line();
 console.log('fooExport - module defines its exports in package.json');
-[
+/** @type TestCase[] */
+const fooExportCases = [
     // default export is in dist, so this algorithm resolves to that folder
     ["fooExport", '/node_modules/fooExport/dist/js'],
     // _styles.scss is the export, so styles (or _styles or styles.scss) resolve to the default export, and then append their path
@@ -44,18 +50,32 @@ console.log('fooExport - module defines its exports in package.json');
     ["fooExport/styles.scss", '/node_modules/fooExport/dist/js/styles.scss'],
     // _styles.scss is a subpath export, so resolves to an exact file
     ["fooExport/_styles.scss", '/node_modules/fooExport/dist/scss/_index.scss']
-].forEach(example => checkExample(example))
+];
+fooExportCases.forEach(checkExample);
 
 line();
 console.log('@bar/foo - namespaced module name\n');
-[["@bar/foo", '/node_modules/@bar/foo']].forEach(example => checkExample(example))
+/** @type TestCase[] */
+const barFooCases = [["@bar/foo", '/node_modules/@bar/foo']];
+barFooCases.forEach(checkExample);
 
 line();
 console.log('badImport - a module that does not exist\n');
-[
+/** @type TestCase[] */
+const badImportCases = [
     ["badImport", null],
     ["badImport/styles.scss", null],
-].forEach(example => checkExample(example))
+];
+badImportCases.forEach(checkExample);
+
+line();
+console.log('noPackage - a package missing a package.json file\n');
+/** @type TestCase[] */
+const noPackageCases = [
+    ["noPackage", null],
+    ["noPackage/_index.scss", '/node_modules/noPackage/_index.scss'],
+];
+noPackageCases.forEach(checkExample);
 
 
 
